43aedfdf57c8c9d39db852a249fd60441b143838
jz-cl <janusz.zychowicz@gmail.com>
2022-09-26 22:42:43 +0200

    add pagination add url delete RestaurantDetailSerializer

--- app_restaurant/serializers.py
+++ app_restaurant/serializers.py
@@ -1,14 +1,15 @@
 from .models import Restaurant
 from rest_framework import serializers
 
-class RestaurantSerializer(serializers.ModelSerializer):
+class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
         class Meta:
             model = Restaurant
-            fields = ['url', 'name', 'city']
+            fields = ['url', 'name', 'city', 'street']
 
 
-class RestaurantDetailSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Restaurant
-        fields = ['name', 'city', 'street', 'house_number', 'phone_number']
+# class RestaurantDetailSerializer(serializers.ModelSerializer):
+# class RestaurantDetailSerializer(serializers.HyperlinkedModelSerializer):
+#
+#     class Meta:
+#         model = Restaurant
+#         fields = ['name', 'city', 'street', 'house_number', 'phone_number']
