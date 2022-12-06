8f719bcba46652d1889387b71f8d2445eb6033f9
natcza <natalia.nc.czajkowska@gmail.com>
2022-12-06 22:51:50 +0100

    update model

--- app_restaurant/serializers.py
+++ app_restaurant/serializers.py
@@ -5,19 +5,16 @@ from rest_framework import serializers
 # class KindSerializer(serializers.ModelSerializer):
 class KindSerializer(serializers.HyperlinkedModelSerializer):
     # restaurants = serializers.PrimaryKeyRelatedField(queryset=Restaurant.objects.all(), many=True)
-    restaurants = serializers.HyperlinkedRelatedField(queryset=Restaurant.objects.all(), many=True, view_name='restaurant-detail')
+    # restaurants = serializers.HyperlinkedRelatedField(queryset=Restaurant.objects.all(), many=True, view_name='restaurant-detail')
     # url = serializers.CharField(source='get_absolute_url', read_only=True)
-    view_name = 'kind'
-    # restaurants = serializers.SlugRelatedField(queryset=Restaurant.objects.all(), many=True,
-    #                                                   slug_field='name')
+    # view_name = 'kind'
+    restaurants = serializers.SlugRelatedField(queryset=Restaurant.objects.all(), many=True,
+                                                      slug_field='name')
     # restaurants = serializers.PrimaryKeyRelatedField(queryset=Restaurant.objects.all(), many=True)
     # view_name='kind-detail'
     class Meta:
         model = Kind
         fields = ['id', 'name', 'restaurants']
-
-
-
 
 class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     kind_list = KindSerializer(many=True, read_only=True)
@@ -25,7 +22,6 @@ class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Restaurant
         fields = ['url', 'name', 'city', 'street', 'house_number', 'phone_number', 'kind_list']
-
 
 
 class SectionSerializer(serializers.HyperlinkedModelSerializer):
